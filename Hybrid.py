5c2af95e3dd06579018c22a40c10552439b6a89d
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2023-11-28 20:36:32 +0100

    normalized hybrid added

--- Hybrid.py
+++ Hybrid.py
@@ -5,6 +5,8 @@ from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
 from Recommenders.KNN.UserKNNCFRecommender import UserKNNCFRecommender
 from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
 
+from numpy import linalg as LA
+import scipy.sparse as sps
 
 # Custom Hybrid class
 
@@ -30,7 +32,7 @@ class HybridRecommender(BaseRecommender):
 
             interactions = len(self.URM_train[user_id_array[i], :].indices)
 
-            if interactions < 4:
+            if interactions < 8:
                 w = self.RP3_recommender._compute_item_score(user_id_array[i], items_to_compute)
                 item_weights[i, :] = w
             else:
@@ -69,3 +71,49 @@ class LinearHybridRecommender(BaseRecommender):
             result = result + self.alphas[index] * self.recommenders[index]._compute_item_score(user_id_array,
                                                                                                 items_to_compute)
         return result
+
+
+
+
+class DifferentLossScoresHybridRecommender(BaseRecommender):
+    """ ScoresHybridRecommender
+    Hybrid of two prediction scores R = R1/norm*alpha + R2/norm*(1-alpha) where R1 and R2 come from
+    algorithms trained on different loss functions.
+
+    """
+
+    RECOMMENDER_NAME = "DifferentLossScoresHybridRecommender"
+
+    def __init__(self, URM_train, recommender_1, recommender_2):
+        super(DifferentLossScoresHybridRecommender, self).__init__(URM_train)
+
+        self.URM_train = sps.csr_matrix(URM_train)
+        self.recommender_1 = recommender_1
+        self.recommender_2 = recommender_2
+
+    def fit(self, norm, alpha=0.5):
+
+        self.alpha = alpha
+        self.norm = norm
+
+    def _compute_item_score(self, user_id_array, items_to_compute):
+
+        item_weights_1 = self.recommender_1._compute_item_score(user_id_array)
+        item_weights_2 = self.recommender_2._compute_item_score(user_id_array)
+
+        norm_item_weights_1 = LA.norm(item_weights_1, self.norm)
+        norm_item_weights_2 = LA.norm(item_weights_2, self.norm)
+
+        if norm_item_weights_1 == 0:
+            raise ValueError(
+                "Norm {} of item weights for recommender 1 is zero. Avoiding division by zero".format(self.norm))
+
+        if norm_item_weights_2 == 0:
+            raise ValueError(
+                "Norm {} of item weights for recommender 2 is zero. Avoiding division by zero".format(self.norm))
+
+        item_weights = item_weights_1 / norm_item_weights_1 * self.alpha + item_weights_2 / norm_item_weights_2 * (
+                    1 - self.alpha)
+
+        return item_weights
+
