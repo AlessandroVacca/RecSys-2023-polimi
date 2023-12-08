b41f864c31b7884a9cd68b335385c8b1872ace84
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2023-12-08 01:08:00 +0100

    finally some improvement

--- Hybrid.py
+++ Hybrid.py
@@ -2,7 +2,8 @@ import numpy as np
 
 from Recommenders.BaseRecommender import BaseRecommender
 from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
-from Recommenders.SLIM.SLIMElasticNetRecommender import SLIMElasticNetRecommender
+from Recommenders.KNN.UserKNNCFRecommender import UserKNNCFRecommender
+from Recommenders.SLIM.SLIMElasticNetRecommender import *
 
 from numpy import linalg as LA
 import scipy.sparse as sps
@@ -17,10 +18,13 @@ class HybridRecommender(BaseRecommender):
         super(HybridRecommender, self).__init__(URM_train)
 
     def fit(self):
-        self.slim_recommender = SLIMElasticNetRecommender(self.URM_train)
+        self.slim_recommender = MultiThreadSLIM_SLIMElasticNetRecommender(self.URM_train)
         self.RP3_recommender = RP3betaRecommender(self.URM_train)
-        self.slim_recommender.fit(topK=8894, l1_ratio=0.05565733019999427, alpha=0.0012979360257937668)
+        self.userknn_recommender = UserKNNCFRecommender(self.URM_train)
+        self.slim_recommender.fit(topK=8894, l1_ratio=0.05565733019999427, alpha=0.0012979360257937668, workers = 7)
         self.RP3_recommender.fit(topK=101, alpha=0.3026342852596128, beta=0.058468783118329024)
+        self.userknn_recommender.fit(topK=469, shrink=38, similarity='asymmetric', normalize=True,
+                                       feature_weighting='TF-IDF', asymmetric_alpha=0.40077406933762383)
 
     def save_model(self, folder_path, file_name=None):
         pass
@@ -32,11 +36,17 @@ class HybridRecommender(BaseRecommender):
 
             interactions = len(self.URM_train[user_id_array[i], :].indices)
 
-            if interactions < 5:
-                w = self.RP3_recommender._compute_item_score(user_id_array[i], items_to_compute)
+            if interactions == 1:
+                w = self.userknn_recommender._compute_item_score(user_id_array[i], items_to_compute)
+                item_weights[i, :] = w
+            # elif interactions == 2: or interactions == 3:
+            #     w = self.RP3_recommender._compute_item_score(user_id_array[i], items_to_compute)
+            #     item_weights[i, :] = w
+            if interactions >= 2 & interactions <= 250:
+                w = self.slim_recommender._compute_item_score(user_id_array[i], items_to_compute)
                 item_weights[i, :] = w
             else:
-                w = self.slim_recommender._compute_item_score(user_id_array[i], items_to_compute)
+                w = self.RP3_recommender._compute_item_score(user_id_array[i], items_to_compute)
                 item_weights[i, :] = w
 
         return item_weights
